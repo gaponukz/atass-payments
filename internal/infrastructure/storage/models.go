923773ebccbf91d565d4810fb2ac0ce667e7ffb8
gaponukz <gaponukz54@gmail.com>
2023-08-07 20:12:07 +0300

    Remove json from db models

--- internal/infrastructure/storage/models.go
+++ internal/infrastructure/storage/models.go
@@ -6,26 +6,26 @@ import (
 )
 
 type passengerModel struct {
-	ID          string `json:"id" gorm:"primaryKey;not null"`
-	Gmail       string `json:"gmail" gorm:"not null"`
-	FullName    string `json:"fullName" gorm:"not null"`
-	PhoneNumber string `json:"phoneNumber" gorm:"not null"`
-	MoveFromID  string `json:"movingFromId" gorm:"not null"`
-	MoveToID    string `json:"movingTowardsId" gorm:"not null"`
+	ID          string `gorm:"primaryKey;not null"`
+	Gmail       string `gorm:"not null"`
+	FullName    string `gorm:"not null"`
+	PhoneNumber string `gorm:"not null"`
+	MoveFromID  string `gorm:"not null"`
+	MoveToID    string `gorm:"not null"`
 }
 
 type paymentModel struct {
-	ID          string           `json:"id" gorm:"primaryKey;not null;unique"`
-	Amount      float64          `json:"amount" gorm:"not null"`
-	RouteID     entities.RouteID `json:"routeId" gorm:"not null"`
-	PassengerID string           `json:"passengerId" gorm:"not null"`
-	Passenger   passengerModel   `json:"passenger" gorm:"foreignKey:PassengerID"`
+	ID          string           `gorm:"primaryKey;not null;unique"`
+	Amount      float64          `gorm:"not null"`
+	RouteID     entities.RouteID `gorm:"not null"`
+	PassengerID string           `gorm:"not null"`
+	Passenger   passengerModel   `gorm:"foreignKey:PassengerID"`
 }
 
 type outboxDataModel struct {
-	CreatedAt   time.Time        `json:"createdAt" gorm:"column:created_at;not null"`
-	PaymentID   string           `json:"paymentId" gorm:"primaryKey;not null;unique"`
-	RouteID     entities.RouteID `json:"routeId" gorm:"not null"`
-	PassengerID string           `json:"passengerId" gorm:"not null"`
-	Passenger   passengerModel   `json:"passenger" gorm:"foreignKey:PassengerID"`
+	CreatedAt   time.Time        `gorm:"column:created_at;not null"`
+	PaymentID   string           `gorm:"primaryKey;not null;unique"`
+	RouteID     entities.RouteID `gorm:"not null"`
+	PassengerID string           `gorm:"not null"`
+	Passenger   passengerModel   `gorm:"foreignKey:PassengerID"`
 }
