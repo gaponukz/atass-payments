cf978f7f67dccac59f8bbd717fad0dc3f29a1098
gaponukz <gaponukz54@gmail.com>
2023-10-20 13:20:00 +0300

    Add submit_payment

--- src/application/usecases/handle_payment.py
+++ src/application/usecases/handle_payment.py
@@ -7,6 +7,9 @@ class PaymentRepository(typing.Protocol):
     def set_status(self, payment_id: str, status: dto.PaymentStatus):
         ...
 
+    def submit_payment(self, payment_id: str):
+        ...
+
 
 class PaymentExternalAPI(typing.Protocol):
     def get_payment_status(self, payment_id: str) -> dto.PaymentStatus:
@@ -19,6 +22,13 @@ class HandlePaymentUseCase:
         self._api = api
 
     def handle(self, payment_id: str, retries: int, wait_before_check: int):
+        try:
+            self._handle(payment_id, retries, wait_before_check)
+
+        finally:
+            self._storage.submit_payment(payment_id)
+
+    def _handle(self, payment_id: str, retries: int, wait_before_check: int):
         for _ in range(retries):
             status = self._api.get_payment_status(payment_id)
 
